7c207875cc1b7a8e3c4540a7dcaede519ecb02e0
後藤 佑太 <made.in.yuta@gmail.com>
2015-04-14 02:48:31 +0900

    スクレイピング変更

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,7 +3,6 @@ class TopController < ReviewController
   before_action :authenticate_user! ,:except => [:index]
 
   def index
-    Scraping.get_products
     @products = Product.order('id ASC').limit(20) # productテーブルから最新順に作品データを20件取り出す
   end
 
